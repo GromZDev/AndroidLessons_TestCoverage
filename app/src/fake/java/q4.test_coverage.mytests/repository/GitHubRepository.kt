b10c7981282fd5710f065d9b7f518b4fb1dfab5d
GromZDev <Dmitryev@mail.ru>
2021-11-02 17:47:56 +0300

    Избавился от BuildConfig.TYPE == FAKE. Теперь файл GitHubRepository берётся автоматически в зависимости от сборки (fake или real). Для этого создаём папки соответствующие и дублируем файл GitHubRepository, а из main его убираем.

--- app/src/fake/java/q4.test_coverage.mytests/repository/GitHubRepository.kt
+++ app/src/fake/java/q4.test_coverage.mytests/repository/GitHubRepository.kt
@@ -5,7 +5,7 @@ import q4.test_coverage.mytests.model.SearchResponse
 import q4.test_coverage.mytests.presenter.RepositoryContract
 import retrofit2.Response
 
-internal class FakeGitHubRepository : RepositoryContract {
+internal class GitHubRepository(private val gitHubApi: GitHubApi) : RepositoryContract {
 
     override fun searchGithub(
         query: String,
